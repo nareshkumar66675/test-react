e88e36f69d610b9c90e8d4a26a38ac47c30e4232
Naresh <nareshkumar66675@gmail.com>
2018-05-30 10:05:16 -0500

    Base Structure

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  devServer: {
+    compress: true,
+    openPage: './dist/index.html',
+    port: 8080
+  },
+  plugins: [htmlPlugin]
+};
